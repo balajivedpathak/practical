9acbd03f2fef6a54d63b72ac75e74dcd75a50c6a
balajivedpathak <127476272+balajivedpathak@users.noreply.github.com>
2023-03-16 10:26:08 +0530

    Add files via upload

---
+++ Perimeter.java
@@ -0,0 +1,10 @@
+class Perimeter{
+public static void main(String []args){
+System.out.println("The two number is :");
+int a = 5;
+int b = 8;
+System.out.println(a + " "+ b);
+int Perimeter = 2 * (a+b);
+System.out.println("The Perimeter is of Rectangle = "+ Perimeter);
+}
+}
---
+++ Rectangle.java
@@ -0,0 +1,10 @@
+class Rectangle{
+public static void main(String []args){
+System.out.println("The two number is :");
+int a = 5;
+int b = 8;
+System.out.println(a + " "+ b);
+int Area = a*b;
+System.out.println("The Area is of Rectangle = "+ Area);
+}
+}
